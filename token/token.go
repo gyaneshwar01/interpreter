bf1374f4bd7529540ec971abcb7a1ff1d900d790
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-10-26 21:47:15 +0545

    basic lexer

--- token/token.go
+++ token/token.go
@@ -32,3 +32,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"let": LET,
+	"fn":  FUNCTION,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
